b29f018c5d974a4f521cf3b0b332f5a1a69a5e0d
Sahil-phcp <sahilatwal2003@gmail.com>
2025-06-17 09:25:15 +0530

    compiling the contract

--- hardhat_env/hardhat.config.ts
+++ hardhat_env/hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { version } from "hardhat";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
 };
+
+module.exports = {
+  solidity : {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },    
+  },
+}
 
 export default config;
